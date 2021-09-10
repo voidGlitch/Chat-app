992f0b7cadd3a017ce158df822380fb83a5ed292
voidGlitch <official.shreshthav@gmail.com>
2021-09-10 14:37:50 +0530

    initialize firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+const config = {
+  apiKey: 'AIzaSyAm6oW7R1momz9P9HNgbKCgsi3ZpkcynMM',
+  authDomain: 'chat-web-app-9eb34.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-9eb34-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-9eb34',
+  storageBucket: 'chat-web-app-9eb34.appspot.com',
+  messagingSenderId: '917375924983',
+  appId: '1:917375924983:web:916b92645ba03bc64131f2',
+};
+
+const app = firebase.initializeApp(config);
