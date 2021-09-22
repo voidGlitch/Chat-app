743c58d0b89157e42109a535388b120018ebb44c
voidGlitch <official.shreshthav@gmail.com>
2021-09-22 22:25:49 +0530

    display real time listeners

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,11 +1,15 @@
-import { useCallback, useState, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+import { database } from './firebase';
 
 export function useModalState(defaultValue = false) {
   const [isOpen, setIsOpen] = useState(defaultValue);
+
   const open = useCallback(() => setIsOpen(true), []);
   const close = useCallback(() => setIsOpen(false), []);
+
   return { isOpen, open, close };
 }
+
 export const useMediaQuery = query => {
   const [matches, setMatches] = useState(
     () => window.matchMedia(query).matches
@@ -23,3 +27,25 @@ export const useMediaQuery = query => {
 
   return matches;
 };
+
+export function usePresence(uid) {
+  const [presence, setPresence] = useState(null);
+
+  useEffect(() => {
+    const userStatusRef = database.ref(`/status/${uid}`);
+
+    userStatusRef.on('value', snap => {
+      if (snap.exists()) {
+        const data = snap.val();
+
+        setPresence(data);
+      }
+    });
+
+    return () => {
+      userStatusRef.off();
+    };
+  }, [uid]);
+
+  return presence;
+}
