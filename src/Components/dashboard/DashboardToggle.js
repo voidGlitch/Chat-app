32cbf3499c1238f04a75de897b1764e5f40f768f
voidGlitch <official.shreshthav@gmail.com>
2021-09-22 17:32:05 +0530

    add realtime presense listner to a user

--- src/Components/dashboard/DashboardToggle.js
+++ src/Components/dashboard/DashboardToggle.js
@@ -1,22 +1,32 @@
 import React, { useCallback } from 'react';
-import { Alert, Button, Drawer, Icon } from 'rsuite';
-import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
+import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
 import Dashboard from '.';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashboardToggle = () => {
-  const { isOpen, open, close } = useModalState();
+  const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
-  const onSignOut = useCallback(() => {
-    auth.signOut();
 
-    Alert.info('Signed Out', 4000);
-    close();
+  const onSignOut = useCallback(() => {
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        Alert.info('Signed out', 4000);
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
   }, [close]);
+
   return (
     <>
       <Button block color="blue" onClick={open}>
-        <Icon icon="dashboard" /> DashboardToggle
+        <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard onSignOut={onSignOut} />
